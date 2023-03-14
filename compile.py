aa82188bf8158096d8565fdde36b802fc839a931
man90 <man90@tuta.io>
2023-03-14 15:44:57 +0100

    Implement appending of characters' data to output file

--- compile.py
+++ compile.py
@@ -4,22 +4,31 @@ from datetime import datetime
 import json
 import os
 
+
+def to_snake(s):
+	return s.lower().replace(" ", "_")
+
+
 json_data = {
-    "version": int(datetime.today().strftime("%y%m%d"))
+	"version": int(datetime.today().strftime("%y%m%d"))
 }
+
+# Load characters' data from JSON
+with open("automatic/characters.genshindev-api.json") as f:
+	chars = json.load(f)
+	json_data["characters"] = dict((to_snake(c["name"]), c) for c in chars)
 
 out_file = "output/data.json"
 
+# Try to backup the previous version and write a new version
 try:
-    # Try to backup the previous version
-    f = open(out_file)
-    old_json_ver = json.load(f)["version"]
-    f.close()
-    os.rename(out_file, f"output/data.v{old_json_ver}.json")
+	with open(out_file) as f:
+		old_ver = json.load(f)["version"]
+
+	os.rename(out_file, f"output/data.v{old_ver}.json")
 except Exception:
-    pass
+	pass
 finally:
-    # Write current version
-    f = open(out_file, "w")
-    f.write(json.dumps(json_data))
-    f.close()
+	f = open(out_file, "w")
+	f.write(json.dumps(json_data))
+	f.close()
