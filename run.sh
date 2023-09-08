f730fc2cd694da79624a359ec6e8d0f76535f885
man90 <man90@tuta.io>
2023-09-08 18:46:04 +0200

    Add virtual environment creation to the run script

--- run.sh
+++ run.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 
+if [[ ! -d ".venv" ]]; then
+	echo 'Installing dependencies'
+
+	python -m venv .venv
+	source .venv/bin/activate
+	pip install -r requirements.txt
+	deactivate
+fi
+
+source .venv/bin/activate
+
+echo 'Updating submodules'
 git submodule foreach 'git pull'
 
+echo 'Running scrapers'
 env python ./genshin-gg-tierlist-scraper.py
 env python ./character-list-extractor.py
 env python ./teams-scraper.py
 env python ./compile.py
+
+deactivate
 
 echo 'data.json generated'
